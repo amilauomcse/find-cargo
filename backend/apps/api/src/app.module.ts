14ca176a7fcaac496385288b8b2eb2266b611a3f
amilauomcse <amila.uom.cse@gmail.com>
2025-03-30 11:27:27 +0530

    file changes

--- backend/apps/api/src/app.module.ts
+++ backend/apps/api/src/app.module.ts
@@ -1,16 +1,17 @@
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from "@nestjs/config";
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST || 'db',
-      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-      username: process.env.DATABASE_USER || 'postgres',
-      password: process.env.DATABASE_PASSWORD || 'postgres',
-      database: process.env.DATABASE_NAME || 'cargo_db',
+      type: "postgres",
+      host: process.env.DATABASE_HOST || "localhost",
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5433,
+      username: process.env.DATABASE_USER || "postgres",
+      password: process.env.DATABASE_PASSWORD || "postgres",
+      database: process.env.DATABASE_NAME || "cargo_db",
       autoLoadEntities: true,
       synchronize: true, // Only for development, disable in production
     }),
