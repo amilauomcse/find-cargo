bf63e38a1d16d44c3b811c074c4c8e0067bc4372
amilauomcse <amila.uom.cse@gmail.com>
2025-04-11 12:06:30 +0530

    console log

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-
+console.log(process.env.REACT_APP_API_BASE_URL);
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3003",
   timeout: 5000,
