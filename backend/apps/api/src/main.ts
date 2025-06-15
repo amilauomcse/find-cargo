bc42568fd38ae20394444b86262b91fc9f98a337
amilauomcse <amila.uom.cse@gmail.com>
2025-06-15 21:53:48 +0530

    cicd

--- backend/apps/api/src/main.ts
+++ backend/apps/api/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://3.233.184.28:3030', //frontend URL
+    origin: 'http://3.233.184.28:5173', //frontend URL
   });
 
   await app.listen(3003);
